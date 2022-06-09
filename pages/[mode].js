735053c7e4921dd15de29e56f05cab786b2f77b4
Meiling Bedard <meiling.bedard@gmail.com>
2022-06-09 12:56:29 -0600

    set up initial focus page with hardcoding

--- pages/[mode].js
+++ pages/[mode].js
@@ -1,13 +1,21 @@
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 
 const Post = () => {
   const router = useRouter()
   const mode = router.query.mode
 
+  const track = '/focus/aspen_migration.mp3'
+
   if (mode === 'focus') {
     return (
       <>
-      focus
+        <Link href='/'>Back</Link>
+        <div>{mode}</div>
+        <div>{track}</div>
+        <audio controls>
+          <source src={track} type='audio/mp3'></source>
+        </audio>
       </>
     )
   } else if (mode === 'relax') {
